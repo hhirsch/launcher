66528e28d481df84106ce72b760d2f6c9222dc86
Henry Hirsch <henry@w3-net.de>
2020-05-30 13:05:00 +0200

    feat: add basic launcher

---
+++ launcher.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+from tkinter import PhotoImage
+from PIL import ImageTk, Image
+import os, subprocess
+from sys import platform
+
+def print_hello():
+    agePath = os.path.normpath('wololo-kingdoms/age2_x1')
+    if platform in ["linux", "linux2"]:
+        subprocess.call(["wine", "WK.exe"], cwd=agePath)
+    else:
+        subprocess.call("WK.exe", cwd=agePath)
+    print('hello')
+
+root = tk.Tk()
+root.geometry("960x600")
+
+#Creates a Tkinter-compatible photo image, which can be used everywhere Tkinter expects an image object.
+annoImg = ImageTk.PhotoImage(Image.open("anno.jpg"))
+aoeImg = ImageTk.PhotoImage(Image.open("aoe2.png"))
+
+annoButton = tk.Button(root, text="anno", image=annoImg, command=print_hello)
+annoButton.grid(row=1,column=1)
+
+aoeButton = tk.Button(root, text="age", image=aoeImg, command=print_hello)
+aoeButton.grid(row=1,column=2)
+
+root.mainloop()
