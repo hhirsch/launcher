e939a2d1213e7b7407c376c3ddcc1f4ef33ad440
Henry Hirsch <henry@w3-net.de>
2020-06-01 12:31:49 +0200

    fix: buttons get properly placed in grid

--- launcher-ui.py
+++ launcher-ui.py
@@ -3,7 +3,7 @@ from tkinter import PhotoImage, Label, messagebox
 from PIL import ImageTk, Image
 import os, subprocess
 from os import path
-import json
+import json, math
 from lib import helper
 from lib.assetexception import AssetException
 from lib.callhelper import runGenericGame
@@ -23,7 +23,7 @@ def addGame(game, data, root):
         invisiblePixel = tk.PhotoImage(width=1, height=1)
         label = Label(image=invisiblePixel)
         label.image = invisiblePixel
-        gameButton = tk.Button(root, text=game, image=invisiblePixel, command=runFunction, height = 206-10, width = 390-10, compound="c")
+        gameButton = tk.Button(root, text=game, image=invisiblePixel, command=runFunction, height = 215-10, width = 460-10, compound="c")
     return gameButton
 
 root = tk.Tk()
@@ -44,12 +44,19 @@ def createMenu(root):
 
     return menu;
 
+currentColumn = 1;
+rowLenght = 4
 for index, content in enumerate(data['games']):
     gameButton = addGame(content, data['games'][content], root)
-    currentRow = (index+1) / 5
+    rawRow = (index+1) / rowLenght
+    currentRow = math.ceil(rawRow)
     menu = createMenu(root)
     showMenu = lambda event: menu.post(event.x_root, event.y_root)
+    print(str(currentRow) + "/" + str(currentColumn))
     gameButton.bind("<Button-3>", showMenu)
-    gameButton.grid(row=int(currentRow), column=index+1)
+    gameButton.grid(row=int(currentRow), column=currentColumn)
+    if currentColumn == rowLenght:
+        currentColumn = 1;
+    currentColumn += 1;
 
 root.mainloop()
