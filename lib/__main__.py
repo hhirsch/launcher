9a825fdde785d1d65f5fc92da7a7721038fcce06
Henry Hirsch <henry@w3-net.de>
2020-06-08 22:46:51 +0200

    feat: add scroll bars

--- lib/__main__.py
+++ lib/__main__.py
@@ -1,10 +1,18 @@
+from tkinter import *
 import tkinter as tk
 import math
 from uihelper import createWindowMenu, addGame
 from ui.launcherwindow import LauncherWindow
+from ui.scrolledwindow import ScrolledWindow
 from config import Config
 
 root = tk.Tk()
+Grid.rowconfigure(root, 0, weight=1)
+Grid.columnconfigure(root, 0, weight=1)
+scrolledWindow = ScrolledWindow(root)
+frame=Frame(scrolledWindow.scrollwindow)
+frame.grid(row=0, column=0, sticky=N+S+E+W)
+
 launcherWindow = LauncherWindow(root)
 cache = False;
 
@@ -23,11 +31,12 @@ except:
 createWindowMenu(root, config.getValue(["launcher", "menu"]))
 currentColumn = 1;
 for index, content in enumerate(config.getValue(["games"])):
-    gameButton = addGame(root, content, config.getValue(["games", content]))
+    gameButton = addGame(frame, content, config.getValue(["games", content]))
     rawRow = (index+1) / rowLength
     currentRow = math.ceil(rawRow)
-    root.rowconfigure(currentRow, weight=5)
-    gameButton.grid(row=int(currentRow), column=currentColumn)
+    frame.rowconfigure(currentRow, weight=1)
+    Grid.rowconfigure(frame, currentRow, weight=1)
+    gameButton.grid(row=int(currentRow), column=currentColumn, sticky=N+S+E+W)
     if currentColumn == rowLength:
         currentColumn = 0;
     currentColumn += 1;
