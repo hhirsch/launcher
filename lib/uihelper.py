b6fc23bef5e0018a0558a63811e7f2279f5d7f49
Henry Hirsch <henry@w3-net.de>
2020-06-17 16:45:43 +0200

    feat: only support images when pil is installed

--- lib/uihelper.py
+++ lib/uihelper.py
@@ -1,8 +1,23 @@
 import tkinter as tk
 from tkinter import ttk
-from PIL import ImageTk, Image
 from helper import getImagePath, gameIsInCache
 from callhelper import runGenericGame, runGenericGameWithStartup
+from assetexception import AssetException
+
+def getImageOrException(game):
+    import importlib
+    pil = importlib.util.find_spec("PIL")
+    found = pil is not None
+
+    if(not found):
+        raise AssetException("Image library not installed!")
+    else:
+        from PIL import ImageTk, Image
+        image = Image.open(getImagePath(game)).convert('RGBA')
+        if not gameIsInCache(game):
+            downloadIconImage = Image.open(getImagePath("download"))
+            image.paste(downloadIconImage)
+        return ImageTk.PhotoImage(image)
 
 def getRunFunction(game, data):
     if "startup" in data:
@@ -13,14 +28,12 @@ def getRunFunction(game, data):
 
 def createButton(root, game, data):
     runFunction = getRunFunction(game, data)
-    image = Image.open(getImagePath(game)).convert('RGBA')
-    if not gameIsInCache(game):
-        downloadIconImage = Image.open(getImagePath("download"))
-        image.paste(downloadIconImage)
-    gameImage = ImageTk.PhotoImage(image)
-    gameButton = tk.Button(root, text=game, image=gameImage, command=runFunction, borderwidth=0)
-    gameButton.image = gameImage
-
+    try:
+        gameImage = getImageOrException(game)
+        gameButton = tk.Button(root, text=game, image=gameImage, command=runFunction, borderwidth=0)
+        gameButton.image = gameImage
+    except AssetException:
+        gameButton = createButtonWithoutImage(root, game, data)
     return gameButton
 
 def createButtonWithoutImage(root, game, data):
